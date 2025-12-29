3e63ff9ec084291df0d19d966f45aec9b1de4db4
hoangcog04 <htvd1204@gmail.com>
2025-12-29 23:18:17 +0700

    chore: remove personal settings config and delegate to .env

--- cnm_bookhub_be/settings.py
+++ cnm_bookhub_be/settings.py
@@ -44,8 +44,8 @@ class Settings(BaseSettings):
     # Variables for the database
     db_host: str = "localhost"
     db_port: int = 3306
-    db_user: str = "root"
-    db_pass: str = ""  # noqa: S105
+    db_user: str = "cnm_bookhub_be"
+    db_pass: str = "cnm_bookhub_be"  # noqa: S105
     db_base: str = "cnm_bookhub_be"
     db_echo: bool = False
 
